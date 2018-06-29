0756ce73c06d114b50c0be4cd2880647b8fcfe36
Altieres de Matos <altitdb@gmail.com>
2018-06-28 23:33:49 -0300

    Add search

--- src/main/angular/src/app/main/search/search.component.ts
+++ src/main/angular/src/app/main/search/search.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+export interface Protocol {
+  entranceDate: Date;
+  exitDate: Date;
+  protocol: string;  
+  sportingPlate: string;
+  originalPlate: string;
+}
+
+const ELEMENT_DATA: Protocol[] = [
+  {entranceDate: new Date(), exitDate: new Date(), protocol: 'PC120318200012018', sportingPlate: 'ASV-7099', originalPlate: ''},
+  {entranceDate: new Date(), exitDate: null, protocol: 'PM120318200012018', sportingPlate: 'ATX-8080', originalPlate: 'ATX-8080'},
+  {entranceDate: new Date(), exitDate: null, protocol: 'PM120318200012018', sportingPlate: 'AAA-9587', originalPlate: 'AAA-9587'}
+];
 
 @Component({
   selector: 'app-search',
@@ -7,9 +22,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  filtred: boolean = false;
+  form: FormGroup;
+  displayedColumns: string[] = ['entranceDate', 'exitDate', 'protocol', 'sportingPlate', 'originalPlate', 'exit'];
+  dataSource = ELEMENT_DATA;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this._formBuilder.group({
+      protocol: '',
+      startDate: '',
+      endDate: ''
+    });
+  }
+
+  search() {
+    this.filtred = true;
   }
 
 }
--- src/main/angular/src/app/main/search/search.module.ts
+++ src/main/angular/src/app/main/search/search.module.ts
@@ -3,12 +3,34 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { RouterModule } from '@angular/router';
 import { SearchRouting } from './search.routing';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material';
+import { MatTableModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    SearchRouting
+    SearchRouting,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatTableModule
   ],
   declarations: [SearchComponent]
 })
