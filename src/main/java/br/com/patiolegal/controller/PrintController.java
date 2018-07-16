fc2526e5c9b386f886f5e2dbd01f61d1376878ca
fernandoWPF <=>
2018-07-15 23:11:45 -0300

    Ajuste em SealController para retornar PDF e ajuste em PrintController

--- src/main/java/br/com/patiolegal/controller/PrintController.java
+++ src/main/java/br/com/patiolegal/controller/PrintController.java
@@ -7,9 +7,8 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patiolegal.exception.PrintException;
@@ -17,7 +16,7 @@ import br.com.patiolegal.exception.PrintException;
 @RestController
 public class PrintController {
 
-    @RequestMapping(value = "/api/v1/print/protocol/{protocol}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/api/v1/print/protocol/{protocol}", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> downloadProtocol(@PathVariable("protocol") String protocol) {
         ClassPathResource pdfFile = new ClassPathResource("protocol.pdf");
         
@@ -38,7 +37,7 @@ public class PrintController {
         }
     }
     
-    @RequestMapping(value = "/api/v1/print/seal/{protocol}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/api/v1/print/seal/{protocol}", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> downloadSeal(@PathVariable("protocol") String protocol) {
         ClassPathResource pdfFile = new ClassPathResource("seal.pdf");
         
--- src/main/java/br/com/patiolegal/controller/SealController.java
+++ src/main/java/br/com/patiolegal/controller/SealController.java
@@ -1,13 +1,19 @@
 package br.com.patiolegal.controller;
 
-import java.io.InputStream;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patiolegal.dto.SealRequestDTO;
+import br.com.patiolegal.exception.PrintException;
 import br.com.patiolegal.service.SealService;
 
 @RestController
@@ -15,10 +21,25 @@ public class SealController {
 
 	@Autowired
 	private SealService service;
-	
-	@PostMapping(path = "/api/v1/print/seal")
-	public InputStream generate(@RequestBody SealRequestDTO request){
-		return service.generate(request);
+
+	@PostMapping(path = "/api/v1/print/seal", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> generate(@RequestBody SealRequestDTO request) {
+		ClassPathResource pdfFile = new ClassPathResource("seal.pdf");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
+		
+		try{
+			return ResponseEntity
+					.ok()
+					.headers(headers)
+					.contentLength(pdfFile.contentLength())
+					.contentType(MediaType.APPLICATION_OCTET_STREAM)
+					.body(new InputStreamResource(service.generate(request)));
+		} catch (IOException e) {
+	        throw new PrintException();
+	    }
 	}
-	
 }
