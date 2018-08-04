21e05f9cc4877fb83e23bc62897dadbef40888fc
fernandoWPF <=>
2018-08-04 16:45:01 -0300

    Criando testes para SearchEntranceResponseDTO

---
+++ src/test/java/br/com/patiolegal/dto/SearchEntranceResponseDTOTest.java
@@ -0,0 +1,57 @@
+package br.com.patiolegal.dto;
+
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.patiolegal.dto.SearchEntranceResponseDTO.SearchEntranceBuilder;
+
+public class SearchEntranceResponseDTOTest {
+
+	@Test
+	public void withDateTimeIn() {
+		LocalDateTime dateTimeIn = LocalDateTime.now();
+
+		SearchEntranceResponseDTO dto = new SearchEntranceBuilder().withDateTimeIn(dateTimeIn).build();
+
+		Assert.assertEquals(dto.getDateTimeIn(), dateTimeIn);
+	}
+
+	@Test
+	public void withDateTimeOut() {
+		LocalDateTime dateTimeOut = LocalDateTime.of(2018, 12, 23, 18, 55);
+
+		SearchEntranceResponseDTO dto = new SearchEntranceBuilder().withDateTimeOut(dateTimeOut).build();
+
+		Assert.assertEquals(dto.getDateTimeOut(), dateTimeOut);
+	}
+
+	@Test
+	public void withProtocol() {
+		String protocol = "PROTOCOLO2015081520568978";
+
+		SearchEntranceResponseDTO dto = new SearchEntranceBuilder().withProtocol(protocol).build();
+
+		Assert.assertEquals(dto.getProtocol(), protocol);
+	}
+
+	@Test
+	public void withSportingPlate() {
+		String sportingPlate = "AAE-9858";
+
+		SearchEntranceResponseDTO dto = new SearchEntranceBuilder().withSportingPlate(sportingPlate).build();
+
+		Assert.assertEquals(dto.getSportingPlate(), sportingPlate);
+	}
+
+	@Test
+	public void withOriginalPlate() {
+		String originalPlate = "AAS-2356";
+
+		SearchEntranceResponseDTO dto = new SearchEntranceBuilder().withOriginalPlate(originalPlate).build();
+
+		Assert.assertEquals(dto.getOriginalPlate(), originalPlate);
+
+	}
+}
