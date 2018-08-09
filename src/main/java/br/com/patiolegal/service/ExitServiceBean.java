4bba4fd06c5d4431d6b2d45125fb5d73618247e8
fernando <fernando.fernandes@matera.com>
2018-08-09 19:52:39 -0300

    Adicionando validação em data de saida

--- src/main/java/br/com/patiolegal/service/ExitServiceBean.java
+++ src/main/java/br/com/patiolegal/service/ExitServiceBean.java
@@ -1,5 +1,6 @@
 package br.com.patiolegal.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -17,33 +18,43 @@ import br.com.patiolegal.repository.EntranceRepository;
 @Service
 public class ExitServiceBean implements ExitService {
 
-    private static final Logger LOG = LogManager.getLogger(ExitServiceBean.class);
+	private static final Logger LOG = LogManager.getLogger(ExitServiceBean.class);
 
-    @Autowired
-    private EntranceRepository repository;
+	@Autowired
+	private EntranceRepository repository;
 
-    @Override
-    public void exit(ExitRequestDTO request) {
-        LOG.debug("Dados recebidos na requisição: " + request);
+	@Override
+	public void exit(ExitRequestDTO request) {
+		LOG.debug("Dados recebidos na requisição: " + request);
 
-        List<Protocol> protocols = repository.findByProtocol(request.getProtocol());
-        if (protocols.isEmpty()) {
-            throw new ProtocolNotFoundException();
-        }
+		List<Protocol> protocols = repository.findByProtocol(request.getProtocol());
+		if (protocols.isEmpty()) {
+			throw new ProtocolNotFoundException();
+		}
 
-        LOG.debug("Protocolo encontrado. Iniciando processo de saida...");
+		LOG.debug("Protocolo encontrado. Iniciando processo de saida...");
 
-        Protocol protocol = protocols.get(0);
-        if (protocol.getExit() != null) {
-            throw new BusinessException("exit", "Protocolo já baixado.");
-        }
+		LOG.debug("Validando date...");
+		validateDate(request);
 
-        Exit exit = new Exit(request.getDate(), request.getTaxId(), request.getName());
-        protocol.setExit(exit);
+		Protocol protocol = protocols.get(0);
+		if (protocol.getExit() != null) {
+			throw new BusinessException("exit", "Protocolo já baixado.");
+		}
 
-        LOG.debug("Efetuando saída.");
-        repository.save(protocol);
-        LOG.debug("Saída efetuada.");
-    }
+		Exit exit = new Exit(request.getDate(), request.getTaxId(), request.getName());
+		protocol.setExit(exit);
+
+		LOG.debug("Efetuando saída.");
+		repository.save(protocol);
+		LOG.debug("Saída efetuada.");
+	}
+
+	private void validateDate(ExitRequestDTO request) {
+		if (request.getDate().isAfter(LocalDate.now())) {
+			throw new BusinessException("date", "Data de saida não pode ser maior que data atual");
+		}
+
+	}
 
 }
