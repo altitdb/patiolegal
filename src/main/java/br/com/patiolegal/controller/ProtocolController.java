9fae49c66a587055bb08a89c5d61ea667712b5eb
Altieres de Matos <altitdb@gmail.com>
2018-08-15 18:20:09 -0300

    Refact controllers

--- src/main/java/br/com/patiolegal/controller/ProtocolController.java
+++ src/main/java/br/com/patiolegal/controller/ProtocolController.java
@@ -1,23 +1,53 @@
 package br.com.patiolegal.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patiolegal.dto.FileIdentifierDTO;
 import br.com.patiolegal.dto.ProtocolRequestDTO;
+import br.com.patiolegal.exception.NotFoundException;
 
 @RestController
 public class ProtocolController {
 
-	@PostMapping(value = "/api/v1/print/protocol")
-	public ResponseEntity<FileIdentifierDTO> generate(@RequestBody @Valid ProtocolRequestDTO request) {
-	    FileIdentifierDTO fileIdentifier = new FileIdentifierDTO("mongodbid");
-	    return new ResponseEntity<>(fileIdentifier, HttpStatus.ACCEPTED);
-	}
+    @PostMapping(value = "/api/v1/print/protocol")
+    public ResponseEntity<FileIdentifierDTO> generate(@RequestBody @Valid ProtocolRequestDTO request) {
+        FileIdentifierDTO fileIdentifier = new FileIdentifierDTO("mongodbid");
+        return new ResponseEntity<>(fileIdentifier, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(value = "/api/v1/print/protocol/{id}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> downloadProtocol(@PathVariable("id") String protocol) {
+        ClassPathResource pdfFile = new ClassPathResource("protocol.pdf");
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        
+        try {
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .contentLength(pdfFile.contentLength())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(pdfFile.getInputStream()));
+        } catch (IOException e) {
+            throw new NotFoundException();
+        }
+    }
 
 }
--- src/main/java/br/com/patiolegal/controller/SealController.java
+++ src/main/java/br/com/patiolegal/controller/SealController.java
@@ -1,22 +1,47 @@
 package br.com.patiolegal.controller;
 
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patiolegal.dto.FileIdentifierDTO;
 import br.com.patiolegal.dto.SealRequestDTO;
+import br.com.patiolegal.exception.NotFoundException;
 
 @RestController
 public class SealController {
 
-	@PostMapping(path = "/api/v1/print/seal", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<FileIdentifierDTO> generate(@RequestBody SealRequestDTO request) {
-	    FileIdentifierDTO fileIdentifier = new FileIdentifierDTO("mongodbid");
+    @PostMapping(path = "/api/v1/print/seal", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FileIdentifierDTO> generate(@RequestBody SealRequestDTO request) {
+        FileIdentifierDTO fileIdentifier = new FileIdentifierDTO("mongodbid");
         return new ResponseEntity<>(fileIdentifier, HttpStatus.ACCEPTED);
-	}
-	
+    }
+
+    @GetMapping(value = "/api/v1/print/seal/{id}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> downloadSeal(@PathVariable("id") String protocol) {
+        ClassPathResource pdfFile = new ClassPathResource("seal.pdf");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        try {
+            return ResponseEntity.ok().headers(headers).contentLength(pdfFile.contentLength())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(pdfFile.getInputStream()));
+        } catch (IOException e) {
+            throw new NotFoundException();
+        }
+    }
 }
