c4a9a3213706269f1baf19c4b43cc853c697b4a7
fernandoWPF <=>
2018-08-20 22:57:05 -0300

    Adicionando authentication em Seal.java

--- src/main/java/br/com/patiolegal/domain/Seal.java
+++ src/main/java/br/com/patiolegal/domain/Seal.java
@@ -1,7 +1,10 @@
 package br.com.patiolegal.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,40 +12,52 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "seal")
 public class Seal {
 
-    @Id
-    private String id;
-    private LocalDate date = LocalDate.now();
-    private String username;
-    private Integer amount;
-    private String reason;
-    private Binary file;
-    
-    public void setFile(Binary file) {
-        this.file = file;
-    }
-    
-    public String getId() {
-        return id;
-    }
+	@Id
+	private String id;
+	private LocalDate date = LocalDate.now();
+	private String username;
+	private Integer amount;
+	private String reason;
+	private Binary file;
+	private String authentication;
 
-    public LocalDate getDate() {
-        return date;
-    }
+	public void setFile(Binary file) {
+		this.file = file;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public Integer getAmount() {
-        return amount;
-    }
+	public LocalDate getDate() {
+		return date;
+	}
 
-    public String getReason() {
-        return reason;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public Binary getFile() {
-        return file;
-    }
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public Binary getFile() {
+		return file;
+	}
+
+	public String getAuthentication() {
+		return authentication;
+	}
+
+	public void generateAuthentication() {
+		LocalDateTime now = LocalDateTime.now();
+		byte[] bytes = now.toString().getBytes();
+		UUID uuid = UUID.nameUUIDFromBytes(bytes);
+		authentication = StringUtils.upperCase(uuid.toString());
+	}
 
 }
