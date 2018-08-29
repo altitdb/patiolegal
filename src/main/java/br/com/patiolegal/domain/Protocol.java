793acf0fec4988346de61ff264d60a080bacfc0b
Altieres de Matos <altitdb@gmail.com>
2018-08-29 20:20:53 -0300

    Adicionando file em Protocol.java

--- src/main/java/br/com/patiolegal/domain/Protocol.java
+++ src/main/java/br/com/patiolegal/domain/Protocol.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
+import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,173 +17,182 @@ import br.com.patiolegal.generator.ProtocolGenerator;
 @Document(collection = "protocol")
 public class Protocol {
 
-	@Id
-	private String id;
-	private String protocol;
-	private String part;
-	private LocalDate date;
-	private LocalDateTime dateTimeIn = LocalDateTime.now();
-	private String policeInvestigation;
-	private String eventBulletin;
-	private String taxIdentifier;
-	private String name;
-	private String originCapture;
-	private String accountableOut;
-	private String accountableIn;
-	private String board;
-	private String authentication;
-	@DBRef
-	private List<Seal> seals = new ArrayList<>();
-	private Entrance entrance;
-	private Exit exit;
-	private Part arrestOrgan;
-	private LocalDateTime modificationDate = LocalDateTime.now();
+    @Id
+    private String id;
+    private String protocol;
+    private String part;
+    private LocalDate date;
+    private LocalDateTime dateTimeIn = LocalDateTime.now();
+    private String policeInvestigation;
+    private String eventBulletin;
+    private String taxIdentifier;
+    private String name;
+    private String originCapture;
+    private String accountableOut;
+    private String accountableIn;
+    private String board;
+    private String authentication;
+    @DBRef
+    private List<Seal> seals = new ArrayList<>();
+    private Entrance entrance;
+    private Exit exit;
+    private Part arrestOrgan;
+    private LocalDateTime modificationDate = LocalDateTime.now();
+    private Binary file;
 
-	public void addSeal(Seal seal) {
-		seals.add(seal);
-	}
+    public void addSeal(Seal seal) {
+        seals.add(seal);
+    }
 
-	public String getProtocol() {
-		return protocol;
-	}
+    public String getProtocol() {
+        return protocol;
+    }
 
-	public String getPart() {
-		return part;
-	}
+    public String getPart() {
+        return part;
+    }
 
-	public void setPart(String part) {
-		this.part = part;
-	}
+    public void setPart(String part) {
+        this.part = part;
+    }
 
-	public LocalDate getDate() {
-		return date;
-	}
+    public LocalDate getDate() {
+        return date;
+    }
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
-	public LocalDateTime getDateTimeIn() {
-		return dateTimeIn;
-	}
+    public LocalDateTime getDateTimeIn() {
+        return dateTimeIn;
+    }
 
-	public String getPoliceInvestigation() {
-		return policeInvestigation;
-	}
+    public String getPoliceInvestigation() {
+        return policeInvestigation;
+    }
 
-	public void setPoliceInvestigation(String policeInvestigation) {
-		this.policeInvestigation = policeInvestigation;
-	}
+    public void setPoliceInvestigation(String policeInvestigation) {
+        this.policeInvestigation = policeInvestigation;
+    }
 
-	public String getEventBulletin() {
-		return eventBulletin;
-	}
+    public String getEventBulletin() {
+        return eventBulletin;
+    }
 
-	public void setEventBulletin(String eventBulletin) {
-		this.eventBulletin = eventBulletin;
-	}
+    public void setEventBulletin(String eventBulletin) {
+        this.eventBulletin = eventBulletin;
+    }
 
-	public String getTaxId() {
-		return taxIdentifier;
-	}
+    public String getTaxId() {
+        return taxIdentifier;
+    }
 
-	public void setTaxIdentifier(String taxIdentifier) {
-		this.taxIdentifier = taxIdentifier;
-	}
+    public void setTaxIdentifier(String taxIdentifier) {
+        this.taxIdentifier = taxIdentifier;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getOriginCapture() {
-		return originCapture;
-	}
+    public String getOriginCapture() {
+        return originCapture;
+    }
 
-	public void setOriginCapture(String originCapture) {
-		this.originCapture = originCapture;
-	}
+    public void setOriginCapture(String originCapture) {
+        this.originCapture = originCapture;
+    }
 
-	public String getAccountableOut() {
-		return accountableOut;
-	}
+    public String getAccountableOut() {
+        return accountableOut;
+    }
 
-	public void setAccountableOut(String accountableOut) {
-		this.accountableOut = accountableOut;
-	}
+    public void setAccountableOut(String accountableOut) {
+        this.accountableOut = accountableOut;
+    }
 
-	public String getAccountableIn() {
-		return accountableIn;
-	}
+    public String getAccountableIn() {
+        return accountableIn;
+    }
 
-	public void setAccountableIn(String accountableIn) {
-		this.accountableIn = accountableIn;
-	}
+    public void setAccountableIn(String accountableIn) {
+        this.accountableIn = accountableIn;
+    }
 
-	public String getBoard() {
-		return board;
-	}
+    public String getBoard() {
+        return board;
+    }
 
-	public void setBoard(String board) {
-		this.board = board;
-	}
+    public void setBoard(String board) {
+        this.board = board;
+    }
 
-	public String getAuthentication() {
-		return authentication;
-	}
+    public String getAuthentication() {
+        return authentication;
+    }
 
-	public List<Seal> getSeals() {
-		return seals;
-	}
+    public List<Seal> getSeals() {
+        return seals;
+    }
 
-	public void setSeals(List<Seal> seals) {
-		this.seals = seals;
-	}
+    public void setSeals(List<Seal> seals) {
+        this.seals = seals;
+    }
 
-	public Entrance getEntrance() {
-		return entrance;
-	}
+    public Entrance getEntrance() {
+        return entrance;
+    }
 
-	public void setEntrance(Entrance entrance) {
-		this.entrance = entrance;
-	}
+    public void setEntrance(Entrance entrance) {
+        this.entrance = entrance;
+    }
 
-	public Exit getExit() {
-		return exit;
-	}
+    public Exit getExit() {
+        return exit;
+    }
 
-	public void setExit(Exit exit) {
-		this.exit = exit;
-	}
+    public void setExit(Exit exit) {
+        this.exit = exit;
+    }
 
-	public Part getArrestOrgan() {
-		return arrestOrgan;
-	}
+    public Part getArrestOrgan() {
+        return arrestOrgan;
+    }
 
-	public void setArrestOrgan(Part arrestOrgan) {
-		this.arrestOrgan = arrestOrgan;
-	}
+    public void setArrestOrgan(Part arrestOrgan) {
+        this.arrestOrgan = arrestOrgan;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public LocalDateTime getModificationDate() {
-		return modificationDate;
-	}
+    public LocalDateTime getModificationDate() {
+        return modificationDate;
+    }
 
-	public void generateProtocol() {
-		ProtocolGenerator generator = new ProtocolGenerator();
-		protocol = generator.generateProtocolNumber(entrance);
-	}
+    public Binary getFile() {
+        return file;
+    }
 
-	public void generateAuthentication() {
-		byte[] bytes = protocol.getBytes();
-		UUID uuid = UUID.nameUUIDFromBytes(bytes);
-		authentication = StringUtils.upperCase(uuid.toString());
-	}
+    public void setFile(Binary file) {
+        this.file = file;
+    }
+
+    public void generateProtocol() {
+        ProtocolGenerator generator = new ProtocolGenerator();
+        protocol = generator.generateProtocolNumber(entrance);
+    }
+
+    public void generateAuthentication() {
+        byte[] bytes = protocol.getBytes();
+        UUID uuid = UUID.nameUUIDFromBytes(bytes);
+        authentication = StringUtils.upperCase(uuid.toString());
+    }
 
 }
