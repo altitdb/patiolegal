4cd14bfb58c67d01074bd5882f2a4e693030806b
Altieres de Matos <altitdb@gmail.com>
2018-08-29 20:23:19 -0300

    Implementando FileIdentifierDTO

--- src/main/java/br/com/patiolegal/controller/ProtocolController.java
+++ src/main/java/br/com/patiolegal/controller/ProtocolController.java
@@ -2,6 +2,7 @@ package br.com.patiolegal.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -15,14 +16,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patiolegal.dto.FileIdentifierDTO;
+import br.com.patiolegal.dto.ProtocolRequestDTO;
 import br.com.patiolegal.exception.NotFoundException;
+import br.com.patiolegal.service.ProtocolService;
 
 @RestController
 public class ProtocolController {
 
+    @Autowired
+    private ProtocolService service;
+    
     @PostMapping(value = "/api/v1/print/protocol")
-    public ResponseEntity<FileIdentifierDTO> generate(@RequestBody String protocol) {
-        FileIdentifierDTO fileIdentifier = new FileIdentifierDTO("mongodbid");
+    public ResponseEntity<FileIdentifierDTO> generateProtocol(@RequestBody ProtocolRequestDTO request) {
+        FileIdentifierDTO fileIdentifier = service.generateProtocol(request);
         return new ResponseEntity<>(fileIdentifier, HttpStatus.ACCEPTED);
     }
     
